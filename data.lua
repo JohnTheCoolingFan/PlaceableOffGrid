5fc0fc5ce4731824d0b71b4b968fc33f2e622acf
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-03-06 00:40:04 +0300

    Added recipe for snapper to grid

--- data.lua
+++ data.lua
@@ -1,20 +1,31 @@
 data:extend({
 	{
-    type = "selection-tool",
-    name = "snapper-to-grid",
-    icon = "__base__/graphics/icons/blueprint.png",
-    icon_size = 32,
-    stack_size = 1,
-    subgroup = "tool",
-    order = "c[automated-construction]-e[snapper-to-grid]",
-    flags = {"goes-to-quickbar"},
-    selection_color = {r = 0.9, g = 0.9, b = 0.9, a = 0.2},
-    alt_selection_color = {r = 0.9, g = 0.9, b = 0.9, a = 0.2},
-    selection_mode = {"buildable-type"},
-    alt_selection_mode = {"buildable-type"},
-    selection_cursor_box_type = "entity",
-    alt_selection_cursor_box_type = "copy",
-    can_be_mod_opened = true,
-    show_in_library = true
-	}
+    	type = "selection-tool",
+    	name = "snapper-to-grid",
+    	icon = "__base__/graphics/icons/blueprint.png",
+    	icon_size = 32,
+    	stack_size = 1,
+    	subgroup = "tool",
+    	order = "c[automated-construction]-e[snapper-to-grid]",
+    	flags = {"goes-to-quickbar"},
+    	selection_color = {r = 0.9, g = 0.9, b = 0.9, a = 0.2},
+    	alt_selection_color = {r = 0.9, g = 0.9, b = 0.9, a = 0.2},
+    	selection_mode = {"buildable-type"},
+    	alt_selection_mode = {"buildable-type"},
+    	selection_cursor_box_type = "entity",
+    	alt_selection_cursor_box_type = "copy",
+    	can_be_mod_opened = true,
+    	show_in_library = true
+	},
+	{
+    	type = "recipe",
+    	name = "snapper-to-grid",
+    	enabled = true,
+    	energy_required = 0.5,
+    	ingredients =
+    	{
+      		{"blueprint", 1}
+    	},
+    	result = "snapper-to-grid"
+  	}
 })
