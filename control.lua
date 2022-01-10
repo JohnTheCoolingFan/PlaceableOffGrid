a608598c17e05d00bcd775a92b17107e2d80d0a4
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-01-10 11:25:24 +0300

    Improved snapping algo

--- control.lua
+++ control.lua
@@ -1,5 +1,17 @@
 local function math_round(num) return math.floor(num+0.5) end
 
+local function get_shifts(entity)
+    local bounding_box = entity.selection_box
+    local dimensions = {
+        bounding_box.right_bottom.x-bounding_box.left_top.x,
+        bounding_box.right_bottom.y-bounding_box.left_top.y
+    }
+    return {
+        (dimensions[1]%2)/2,
+        (dimensions[2]%2)/2
+    }
+end
+
 script.on_event(defines.events.on_player_selected_area, function(event)
 
 	-- Check if this is snap-tool
@@ -14,8 +26,9 @@ script.on_event(defines.events.on_player_selected_area, function(event)
 	for _, entity in pairs(entities) do
 		if entity.force == player.force then
 			local position = entity.position
-			--player.print(position.x.." "..position.y)
-			entity.teleport({x = math_round(position.x), y = math_round(position.y)})
+            local shifts = get_shifts(entity)
+            --player.print(position.x.." "..position.y)
+            entity.teleport({x = math_round(position.x + shifts[1]) - shifts[1], y = math_round(position.y - shifts[2]) + shifts[2]})
 			entity_num = entity_num + 1
 		end
 	end
