f2217209c2a810083228b68b24dd84d9f05c5e0d
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-01-10 10:43:47 +0300

    Better variable naming

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -11,20 +11,20 @@ local function has_value(tab, val)
 end
 
 local function place_off_grid(dataType)
-	for _, e in pairs(data.raw[dataType]) do
-        if not e.flags then
-            e.flags = {}
+	for _, entity_prototype in pairs(data.raw[dataType]) do
+        if not entity_prototype.flags then
+            entity_prototype.flags = {}
         end
-		local flags = e.flags
+		local flags = entity_prototype.flags
 		if not has_value(flags, "placeable-off-grid") then
 			table.insert(flags, "placeable-off-grid")
-			e.flags = flags
+			entity_prototype.flags = flags
 		end
 	end
 end
 
-for _, e in pairs(entity_list) do
-	if not settings.startup["pof-disable-"..e].value then
-		place_off_grid(e)
+for _, entity_type in pairs(entity_list) do
+	if not settings.startup["pof-disable-"..entity_type].value then
+		place_off_grid(entity_type)
 	end
 end
