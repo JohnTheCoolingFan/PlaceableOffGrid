9478cf57640848de7db34809108335ff6fa8aca7
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-01-10 10:54:59 +0300

    Code improvements

--- control.lua
+++ control.lua
@@ -1,17 +1,17 @@
 local function math_round(num) return math.floor(num+0.5) end
 
 script.on_event(defines.events.on_player_selected_area, function(event)
-	local player = game.players[event.player_index]
 
 	-- Check if this is snap-tool
 	if event.item ~= "snap-tool" then return end
 
 	-- Setting up some variables
+	local player = game.players[event.player_index]
 	local entities = event.entities
 	local entity_num = 0
 
 	-- Script itself
-	for i, entity in pairs(entities) do
+	for _, entity in pairs(entities) do
 		if entity.force == player.force then
 			local position = entity.position
 			--player.print(position.x.." "..position.y)
