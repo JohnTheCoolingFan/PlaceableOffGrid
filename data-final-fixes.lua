43933537b9b0f7fdca5ed72c4bab2bbaeacc14dd
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-10-30 17:24:43 +0300

    Formatting and fix for empty categories

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,7 @@
 local entity_list = require("entity-list")
 
 local function has_value(tab, val)
-	for _, value in ipairs(tab) do
+    for _, value in ipairs(tab) do
         if value == val then
             return true
         end
@@ -11,17 +11,22 @@ local function has_value(tab, val)
 end
 
 local function add_pog_flag(dataType)
-	for _, entity_prototype in pairs(data.raw[dataType]) do
-        if not entity_prototype.flags then
-            entity_prototype.flags = {"placeable-off-grid"}
-        elseif not has_value(entity_prototype.flags, "placeable-off-grid") then
-            table.insert(entity_prototype.flags, "placeable-off-grid")
+    if data.raw[dataType] then
+        log("Processing prototype category `" .. dataType .. "`")
+        for _, entity_prototype in pairs(data.raw[dataType]) do
+            if not entity_prototype.flags then
+                entity_prototype.flags = { "placeable-off-grid" }
+            elseif not has_value(entity_prototype.flags, "placeable-off-grid") then
+                table.insert(entity_prototype.flags, "placeable-off-grid")
+            end
         end
-	end
+    else
+        log("Empty prototype category: `" .. dataType .. "`")
+    end
 end
 
 for _, entity_type in pairs(entity_list) do
-	if not settings.startup["pof-disable-"..entity_type].value then
-		add_pog_flag(entity_type)
-	end
+    if not settings.startup["pof-disable-" .. entity_type].value then
+        add_pog_flag(entity_type)
+    end
 end
