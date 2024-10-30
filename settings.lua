695ce219b60d82fe91ef62a0946d43e55d7df5a2
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-10-30 17:58:18 +0300

    Formatting

--- settings.lua
+++ settings.lua
@@ -1,14 +1,14 @@
 local entity_list = require("entity-list")
 local result = {}
 
-for i, e in pairs(entity_list) do
-	table.insert(result, 
+for _, e in pairs(entity_list) do
+	table.insert(result,
 		{
 			type = "bool-setting",
-			name = "pof-disable-"..e,
+			name = "pof-disable-" .. e,
 			setting_type = "startup",
-			localised_name = {"mod-setting-name.pof-disable-name", e},
-			localised_description = {"mod-setting-description.pof-disable-description", e},
+			localised_name = { "mod-setting-name.pof-disable-name", e },
+			localised_description = { "mod-setting-description.pof-disable-description", e },
 			default_value = false
 		}
 	)
