de2e7d8c28292a5d12b92c3a6f262829d4beb768
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-10-30 17:49:24 +0300

    Updated prototypes for Factorio 2.0

--- data.lua
+++ data.lua
@@ -10,54 +10,27 @@ data:extend({
 		subgroup = "tool",
 		order = "c[automated-construction]-e[snapper-to-grid]",
 		flags = { 'spawnable' },
-		selection_color = { r = 0.9, g = 0.9, b = 0.9, a = 0.2 },
-		alt_selection_color = { r = 0.9, g = 0.9, b = 0.9, a = 0.2 },
-		selection_mode = { "buildable-type", 'same-force' },
-		alt_selection_mode = { "buildable-type", 'same-force' },
-		selection_cursor_box_type = "entity",
-		alt_selection_cursor_box_type = "copy",
-		can_be_mod_opened = true,
+		select = {
+			border_color = { r = 0.9, g = 0.9, b = 0.9, a = 0.2 },
+			mode = { "buildable-type", 'same-force' },
+			cursor_box_type = "entity",
+		},
+		alt_select = {
+			border_color = { r = 0.9, g = 0.9, b = 0.9, a = 0.2 },
+			mode = { "buildable-type", 'same-force' },
+			cursor_box_type = "copy",
+		}
 	},
-	--[[ No need for recipe in current Factorio design
-	{
-    	type = "recipe",
-    	name = "snap-tool",
-    	enabled = true,
-    	energy_required = 0.5,
-    	ingredients =
-    	{
-      		{"blueprint", 1}
-    	},
-    	result = "snap-tool"
-  	},]]
 	{
 		type = "shortcut",
 		name = "snap-tool",
 		action = "spawn-item",
 		item_to_spawn = "snap-tool",
-		icon =
-		{
-			filename = "__PlaceableOffGrid__/graphics/icons/snap-tool.png",
-			priority = "extra-high-no-scale",
-			size = 32,
-			scale = 1,
-			flags = { "gui-icon" }
-		},
-		small_icon =
-		{
-			filename = "__PlaceableOffGrid__/graphics/icons/snap-tool.png",
-			priority = "extra-high-no-scale",
-			size = 32,
-			scale = 0.75,
-			flags = { "gui-icon" }
-		},
-		disabled_small_icon =
-		{
-			filename = "__PlaceableOffGrid__/graphics/icons/snap-tool.png",
-			priority = "extra-high-no-scale",
-			size = 32,
-			scale = 0.75,
-			flags = { "gui-icon" }
-		},
+		style = "default",
+		order = "b[blueprint]-h[snap-tool]",
+		icon = "__PlaceableOffGrid__/graphics/icons/new-snap-tool-32.png",
+		icon_size = 32,
+		small_icon = "__PlaceableOffGrid__/graphics/icons/new-snap-tool-24.png",
+		small_icon_size = 24
 	}
 })
